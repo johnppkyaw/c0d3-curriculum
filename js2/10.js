e7c0f692038549dda6e16b42bd19572777a150fc
John <jjpyae@gmail.com>
2023-11-17 22:23:55 -0800

    solves problem 10

--- js2/10.js
+++ js2/10.js
@@ -5,8 +5,14 @@
  */
 
 const solution = () => {
-  Array.prototype.cFilter = function () {
-    return 0
+  Array.prototype.cFilter = function (cb, element, index = 0, arr = this, filteredArr = []) {
+    if(arr.length === index) return filteredArr;
+
+    element = element || arr[index];
+    if(cb(element, index, arr) === true) filteredArr.push(element);
+    index += 1;
+
+    return this.cFilter(cb, arr[index], index, arr, filteredArr);
   }
 }
 
